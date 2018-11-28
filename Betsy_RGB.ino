de239ad6cd647eaf7ad4120ec9f1126a518777b4
Michael Jedynak <michaeljedynak@Michaels-MacBook-Pro-2.local>
2018-11-27 22:35:49 -0800

    Code updated to incorporate 4 RGB LED Color Signals. 12 of the Arduino's PWM pins in total send signal to the LEDs. More specifically, an R signal, G signal, and B signal to each of the 4 LEDs.

--- Betsy_RGB.ino
+++ Betsy_RGB.ino
@@ -1,15 +1,35 @@
 #include <RGB_LED.h> //library to control individual RGB LEDs (common Cathode)
 
-#define Pin_b1 3 //RGB (Blue) LED output pin
-#define Pin_g1 5 //RGB (Green) LED output pin
-#define Pin_r1 6 // RGB (Red) LED output pin
-#define Pin_button 8 // Button input pin
+/*NOTES ON PIN ASSIGNMENT:
+12 of the arduino Mega PWM outputs are used to create 4 independent LED Color Signals (3 pins -R, G, and B-  for each LED signal).
+In physical terms, 4 different colors can be shown at once across the total number of LEDs. For example, 4 LEDs can show
+4 different colors at a given time (one for each LED Color Signal) and 8 LEDs also can also only show 4 different 
+colors at a given time (two for each LED Color Signal). 
+*/
+#define Pin_r1 2 //Color Signal 1 (RED) output pin
+#define Pin_g1 3 //Color Signal 1 (GREEN) output pin
+#define Pin_b1 4 //Color Signal 1 (BLUE) output pin
+#define Pin_r2 5 //Color Signal 2 (RED) output pin
+#define Pin_g2 6 //Color Signal 2 (GREEN) output pin
+#define Pin_b2 7 //Color Signal 2 (BLUE) output pin
+#define Pin_r3 8 //Color Signal 3 (RED) output pin
+#define Pin_g3 9 //Color Signal 3 (GREEN) output pin
+#define Pin_b3 10 //Color Signal 3 (BLUE) output pin
+#define Pin_r4 11 //Color Signal 4 (RED) output pin
+#define Pin_g4 12 //Color Signal 4 (GREEN) output pin
+#define Pin_b4 13 //Color Signal 4 (BLUE) output pin
+byte all_rPins[] = {2,5,8,11}; // group all RED pins into one array
+byte all_gPins[] = {3,6,9,12}; //group all GREEN pins into one array
+byte all_bPins[] = {4,7,10,13}; //group all BLUE pins into one array
+byte all_colorPins[] = {2,3,4,5,6,7,8,9,10,11,12,13}; //group all LED RGB pins into one array (useful for turning all LEDs "off")
+
+#define Pin_button 1 // light show "Mode"Button input pin
 
 // -------- Pin Output Variables -----------
 uint8_t maxBrightness; //value 0-255
 
 // --------- Pin Input Variables -----------
-bool pinButtonValue; 
+bool pinButtonValue; //is "Mode" button being pressed or not?
 
 // ----------- State Variables --------------
 uint8_t lightShow_State; //light show's numerical identifier
@@ -21,17 +41,18 @@ uint8_t numberOfStates; //constant that equals the number of light shows program
 void setup() {
   pinMode(Pin_button, INPUT_PULLUP); //INPUT_PULLUP connects the pin to 5V with the arduino's built in 20K resistor in between
   
-  maxBrightness = 0; //0 is max for common anode RGB (255 for common cathode RGB)
+  maxBrightness = 255; //255 is max for common cathode RGB (0 for common anode RGB)
   lightShow_State = 0; //initialize to 0, which is the "All LEDs OFF" state
   numberOfStates = 2; //constant that equals the number of light shows programmed (including the "ALL LEDs OFF" show)
   
   Serial.begin(9600); //for debugging
-
-  //LED1.set(255,255,255); //common anode RGB LED so this sets led to OFF
 }
 
 void loop() {
-  if (lightShow_State == 0){
+  LightShow_0();
+  delay(1000);
+  LightShow_1();
+  /*if (lightShow_State == 0){
     LightShow_0();
   }
   
@@ -47,36 +68,63 @@ void loop() {
         
   if (pinButtonValue == LOW){
     lightShow_State = 1;   
-  }
+   
+  }*/
 }
 
 // ------------- Light Shows (States) ----------------
-void LightShow_0() {
-  analogWrite(Pin_r1,255);
-  analogWrite(Pin_g1,255);
-  analogWrite(Pin_b1,255);
+void LightShow_0() { //turns all LEDs "off"
+  uint16_t n; //declare indexing variable
+  
+  for (n=0; n<12; n++){
+    analogWrite (all_colorPins[n], 0);
+  }
 }
 
-void LightShow_1() {
-  analogWrite(Pin_r1, maxBrightness);
+void LightShow_1() { 
+  uint8_t n; //declare indexing variable
+  
+  for (n=0; n<4; n++){
+    analogWrite (all_rPins[n], maxBrightness);
+  }
   delay(500);
-  analogWrite(Pin_r1, 255);
-  analogWrite(Pin_g1, maxBrightness);
+  for (n=0; n<4; n++){
+    analogWrite (all_rPins[n], 0);
+  }
+  
+  for (n=0; n<4; n++){
+    analogWrite (all_gPins[n], maxBrightness);
+  }
   delay(500);
-  analogWrite(Pin_g1, 255);
-  analogWrite(Pin_b1, maxBrightness);
+  for (n=0; n<4; n++){
+    analogWrite (all_gPins[n], 0);
+  }
+  
+  for (n=0; n<4; n++){
+    analogWrite (all_bPins[n], maxBrightness);
+  }
   delay(500);
-  analogWrite(Pin_b1,255);
+  for (n=0; n<4; n++){
+    analogWrite (all_bPins[n], 0);
+  }
       
-  analogWrite(Pin_r1, maxBrightness);
+  for (n=0; n<4; n++){
+    analogWrite (all_rPins[n], maxBrightness);
+  }
   delay(500);
-  analogWrite(Pin_g1, maxBrightness);
+
+  for (n=0; n<4; n++){
+    analogWrite (all_gPins[n], maxBrightness);
+  }
   delay(500);
-  analogWrite(Pin_b1, maxBrightness);
+  
+  for (n=0; n<4; n++){
+    analogWrite (all_bPins[n], maxBrightness);
+  }
   delay(500);
       
-  analogWrite(Pin_r1, 255);
-  analogWrite(Pin_g1, 255);
-  analogWrite(Pin_b1, 255);
+for (n=0; n<12; n++){
+    analogWrite (all_colorPins[n], 0);
+  }
 }
 
